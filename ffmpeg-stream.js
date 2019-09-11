b7c3b43389167387948c891ffe5a2d3c7ac009d4
Nick <bourdakos1@gmail.com>
2019-09-11 10:52:35 -0400

    🤞

--- ffmpeg-stream.js
+++ ffmpeg-stream.js
@@ -29,38 +29,163 @@ module.exports = RED => {
   var listenerNodes = {}
   var activeListenerNodes = 0
 
-  // A node red node that sets up a local websocket server
-  function WebSocketListenerNode(n) {
-    // Create a RED node
-    RED.nodes.createNode(this, n)
-    var node = this
+  // // A node red node that sets up a local websocket server
+  // function WebSocketListenerNode(n) {
+  //   // Create a RED node
+  //   RED.nodes.createNode(this, n)
+  //   var node = this
 
-    // Store local copies of the node configuration (as defined in the .html)
-    node.path = n.path
-    node.wholemsg = n.wholemsg === 'true'
+  //   // Store local copies of the node configuration (as defined in the .html)
+  //   node.path = n.path
+  //   node.wholemsg = n.wholemsg === 'true'
 
-    node._inputNodes = [] // collection of nodes that want to receive events
-    node._clients = {}
-    // match absolute url
-    node.isServer = !/^ws{1,2}:\/\//i.test(node.path)
-    node.closing = false
-    node.tls = n.tls
+  //   node._inputNodes = [] // collection of nodes that want to receive events
+  //   node._clients = {}
+  //   // match absolute url
+  //   node.isServer = !/^ws{1,2}:\/\//i.test(node.path)
+  //   node.closing = false
+  //   node.tls = n.tls
 
-    function startconn() {
-      // Connect to remote endpoint
-      node.tout = null
-      var options = {}
-      if (node.tls) {
-        var tlsNode = RED.nodes.getNode(node.tls)
-        if (tlsNode) {
-          tlsNode.addTLSOptions(options)
-        }
-      }
-      var socket = new ws(node.path, options)
-      socket.setMaxListeners(0)
-      node.server = socket // keep for closing
-      handleConnection(socket)
-    }
+  //   function startconn() {
+  //     // Connect to remote endpoint
+  //     node.tout = null
+  //     var options = {}
+  //     if (node.tls) {
+  //       var tlsNode = RED.nodes.getNode(node.tls)
+  //       if (tlsNode) {
+  //         tlsNode.addTLSOptions(options)
+  //       }
+  //     }
+  //     var socket = new ws(node.path, options)
+  //     socket.setMaxListeners(0)
+  //     node.server = socket // keep for closing
+  //     handleConnection(socket)
+  //   }
+
+  //   function handleConnection(/*socket*/ socket) {
+  //     var id = (1 + Math.random() * 4294967295).toString(16)
+  //     if (node.isServer) {
+  //       node._clients[id] = socket
+  //       node.emit('opened', {
+  //         count: Object.keys(node._clients).length,
+  //         id: id
+  //       })
+  //     }
+  //     socket.on('open', function() {
+  //       if (!node.isServer) {
+  //         node.emit('opened', { count: '', id: id })
+  //       }
+  //     })
+  //     socket.on('close', function() {
+  //       if (node.isServer) {
+  //         delete node._clients[id]
+  //         node.emit('closed', {
+  //           count: Object.keys(node._clients).length,
+  //           id: id
+  //         })
+  //       } else {
+  //         node.emit('closed', { count: '', id: id })
+  //       }
+  //       if (!node.closing && !node.isServer) {
+  //         clearTimeout(node.tout)
+  //         node.tout = setTimeout(function() {
+  //           startconn()
+  //         }, 3000) // try to reconnect every 3 secs... bit fast ?
+  //       }
+  //     })
+  //     socket.on('message', function(data, flags) {
+  //       node.handleEvent(id, socket, 'message', data, flags)
+  //     })
+  //     socket.on('error', function(err) {
+  //       node.emit('erro', { err: err, id: id })
+  //       if (!node.closing && !node.isServer) {
+  //         clearTimeout(node.tout)
+  //         node.tout = setTimeout(function() {
+  //           startconn()
+  //         }, 3000) // try to reconnect every 3 secs... bit fast ?
+  //       }
+  //     })
+  //   }
+
+  //   if (node.isServer) {
+  //     activeListenerNodes++
+  //     if (!serverUpgradeAdded) {
+  //       RED.server.on('upgrade', handleServerUpgrade)
+  //       serverUpgradeAdded = true
+  //     }
+
+  //     var path = RED.settings.httpNodeRoot || '/'
+  //     path =
+  //       path +
+  //       (path.slice(-1) == '/' ? '' : '/') +
+  //       (node.path.charAt(0) == '/' ? node.path.substring(1) : node.path)
+  //     node.fullPath = path
+
+  //     if (listenerNodes.hasOwnProperty(path)) {
+  //       node.error(
+  //         RED._('websocket.errors.duplicate-path', { path: node.path })
+  //       )
+  //       return
+  //     }
+  //     listenerNodes[node.fullPath] = node
+  //     var serverOptions = {
+  //       noServer: true
+  //     }
+  //     if (RED.settings.webSocketNodeVerifyClient) {
+  //       serverOptions.verifyClient = RED.settings.webSocketNodeVerifyClient
+  //     }
+  //     // Create a WebSocket Server
+  //     node.server = new ws.Server(serverOptions)
+  //     node.server.setMaxListeners(0)
+  //     node.server.on('connection', handleConnection)
+  //   } else {
+  //     node.closing = false
+  //     startconn() // start outbound connection
+  //   }
+
+  //   node.on('close', function() {
+  //     if (node.isServer) {
+  //       delete listenerNodes[node.fullPath]
+  //       node.server.close()
+  //       node._inputNodes = []
+  //       activeListenerNodes--
+  //       // if (activeListenerNodes === 0 && serverUpgradeAdded) {
+  //       //     RED.server.removeListener('upgrade', handleServerUpgrade);
+  //       //     serverUpgradeAdded = false;
+  //       // }
+  //     } else {
+  //       node.closing = true
+  //       node.server.close()
+  //       if (node.tout) {
+  //         clearTimeout(node.tout)
+  //         node.tout = null
+  //       }
+  //     }
+  //   })
+  // }
+
+  // WebSocketListenerNode.prototype.broadcast = function(data) {
+  //   try {
+  //     if (this.isServer) {
+  //       for (let client in this._clients) {
+  //         if (this._clients.hasOwnProperty(client)) {
+  //           this._clients[client].send(data)
+  //         }
+  //       }
+  //     } else {
+  //       this.server.send(data)
+  //     }
+  //   } catch (e) {
+  //     // swallow any errors
+  //   }
+  // }
+
+  function ObjectDetectionNode(config) {
+    RED.nodes.createNode(this, config)
+    this.path = config.path
+    const node = this
+
+    ////////////////////////////////////////////////////////////////////////////
 
     function handleConnection(/*socket*/ socket) {
       var id = (1 + Math.random() * 4294967295).toString(16)
@@ -107,146 +232,44 @@ module.exports = RED => {
       })
     }
 
-    if (node.isServer) {
-      activeListenerNodes++
-      if (!serverUpgradeAdded) {
-        RED.server.on('upgrade', handleServerUpgrade)
-        serverUpgradeAdded = true
-      }
-
-      var path = RED.settings.httpNodeRoot || '/'
-      path =
-        path +
-        (path.slice(-1) == '/' ? '' : '/') +
-        (node.path.charAt(0) == '/' ? node.path.substring(1) : node.path)
-      node.fullPath = path
-
-      if (listenerNodes.hasOwnProperty(path)) {
-        node.error(
-          RED._('websocket.errors.duplicate-path', { path: node.path })
-        )
-        return
-      }
-      listenerNodes[node.fullPath] = node
-      var serverOptions = {
-        noServer: true
-      }
-      if (RED.settings.webSocketNodeVerifyClient) {
-        serverOptions.verifyClient = RED.settings.webSocketNodeVerifyClient
-      }
-      // Create a WebSocket Server
-      node.server = new ws.Server(serverOptions)
-      node.server.setMaxListeners(0)
-      node.server.on('connection', handleConnection)
-    } else {
-      node.closing = false
-      startconn() // start outbound connection
+    if (!serverUpgradeAdded) {
+      RED.server.on('upgrade', handleServerUpgrade)
+      serverUpgradeAdded = true
     }
 
-    node.on('close', function() {
-      if (node.isServer) {
-        delete listenerNodes[node.fullPath]
-        node.server.close()
-        node._inputNodes = []
-        activeListenerNodes--
-        // if (activeListenerNodes === 0 && serverUpgradeAdded) {
-        //     RED.server.removeListener('upgrade', handleServerUpgrade);
-        //     serverUpgradeAdded = false;
-        // }
-      } else {
-        node.closing = true
-        node.server.close()
-        if (node.tout) {
-          clearTimeout(node.tout)
-          node.tout = null
-        }
-      }
-    })
-  }
+    var path = RED.settings.httpNodeRoot || '/'
+    path =
+      path +
+      (path.slice(-1) == '/' ? '' : '/') +
+      (node.path.charAt(0) == '/' ? node.path.substring(1) : node.path)
+    node.fullPath = path
 
-  RED.nodes.registerType('websocket-client2', WebSocketListenerNode)
+    console.log(node.fullPath)
 
-  WebSocketListenerNode.prototype.registerInputNode = function(
-    /*Node*/ handler
-  ) {
-    this._inputNodes.push(handler)
-  }
-
-  WebSocketListenerNode.prototype.removeInputNode = function(/*Node*/ handler) {
-    this._inputNodes.forEach(function(node, i, inputNodes) {
-      if (node === handler) {
-        inputNodes.splice(i, 1)
-      }
-    })
-  }
-
-  WebSocketListenerNode.prototype.handleEvent = function(
-    id,
-    /*socket*/ socket,
-    /*String*/ event,
-    /*Object*/ data,
-    /*Object*/ flags
-  ) {
-    var msg
-    if (this.wholemsg) {
-      try {
-        msg = JSON.parse(data)
-      } catch (err) {
-        msg = { payload: data }
-      }
-    } else {
-      msg = {
-        payload: data
-      }
+    if (listenerNodes.hasOwnProperty(path)) {
+      node.error(RED._('websocket.errors.duplicate-path', { path: node.path }))
+      return
     }
-    msg._session = { type: 'websocket', id: id }
-    for (var i = 0; i < this._inputNodes.length; i++) {
-      this._inputNodes[i].send(msg)
+    listenerNodes[node.fullPath] = node
+    var serverOptions = {
+      noServer: true
     }
-  }
-
-  WebSocketListenerNode.prototype.broadcast = function(data) {
-    try {
-      if (this.isServer) {
-        for (let client in this._clients) {
-          if (this._clients.hasOwnProperty(client)) {
-            this._clients[client].send(data)
-          }
-        }
-      } else {
-        this.server.send(data)
-      }
-    } catch (e) {
-      // swallow any errors
-      this.warn('ws:' + i + ' : ' + e)
+    if (RED.settings.webSocketNodeVerifyClient) {
+      serverOptions.verifyClient = RED.settings.webSocketNodeVerifyClient
     }
-  }
-
-  WebSocketListenerNode.prototype.reply = function(id, data) {
-    var session = this._clients[id]
-    if (session) {
-      try {
-        session.send(data)
-      } catch (e) {
-        // swallow any errors
-      }
-    }
-  }
-
-  function ObjectDetectionNode(config) {
-    RED.nodes.createNode(this, config)
-    this.path = config.path
-    const node = this
-
-    this.serverConfig = RED.nodes.getNode('websocket-client2')
-    console.log(this.serverConfig)
+    // Create a WebSocket Server
+    node.server = new ws.Server(serverOptions)
+    node.server.setMaxListeners(0)
+    node.server.on('connection', handleConnection)
+    ////////////////////////////////////////////////////////////////////////////
 
     node.status({ fill: 'grey', shape: 'ring', text: 'model loading...' })
 
     RED.httpNode.post('/tellostream', (req, res) => {
       res.connection.setTimeout(0)
       req.on('data', data => {
-        node.serverConfig.broadcast(data)
+        console.log('serving data')
+        node.broadcast(data)
       })
     })
 
@@ -268,6 +291,22 @@ module.exports = RED => {
       '20',
       `http://${HOST}:${PORT}/tellostream`
     ])
+  }
+
+  ObjectDetectionNode.prototype.broadcast = function(data) {
+    try {
+      if (this.isServer) {
+        for (let client in this._clients) {
+          if (this._clients.hasOwnProperty(client)) {
+            this._clients[client].send(data)
+          }
+        }
+      } else {
+        this.server.send(data)
+      }
+    } catch (e) {
+      // swallow any errors
+    }
   }
 
   RED.nodes.registerType('ffmpeg-stream', ObjectDetectionNode)
